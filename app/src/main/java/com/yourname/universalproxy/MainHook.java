cc292d10ca95f5c1fe4a1d55aed24f7c1e885370
Harper Richard <harperrichardjr@gmail.com>
2025-11-25 18:07:10 -0800

    Add files via upload

---
+++ app/src/main/java/com/yourname/universalproxy/MainHook.java
@@ -0,0 +1,11 @@
+package com.yourname.universalproxy;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        // Universal Proxy Module
+    }
+}
